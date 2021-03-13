0c4a91bcd12250f12d75c72c67a9776a23674498
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-03-14 01:00:41 +0200

    fix desc and add uninstaller

--- Casks/izotope-product-portal.rb
+++ Casks/izotope-product-portal.rb
@@ -3,7 +3,7 @@ cask "izotope-product-portal" do
   sha256 "111db6bcef7702160449021c793959225a84c182fa9eaf069a0453c7d11f53a0"
   url "https://s3.amazonaws.com/izotopedownloads/product_download/iZotope_Product_Portal_v#{version.major}_#{version.minor}_#{version.patch}.dmg", verified: "amazonaws.com/izotopedownloads"
   name "iZotope Product Portal"
-  desc "Application that allows you to seamlessly manage your iZotope software in one central location"
+  desc "Application that allows you to manage your iZotope software."
   homepage "https://support.izotope.com/hc/en-us/articles/360025586834-Welcome-to-iZotope-Product-Portal"
 
   auto_updates true
@@ -13,4 +13,8 @@ cask "izotope-product-portal" do
     args:       ["--mode", "unattended"],
     sudo:       true,
   }
+
+  uninstall quit:    "com.izotope.productportal.app",
+            delete:  "/Applications/iZotope Product Portal.app"
+
 end
