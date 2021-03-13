f2ece982fcb5e94be0f908bf7575d5a3b2b0d1aa
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-03-14 00:39:35 +0200

    Add izotope product portal cask

---
+++ Casks/izotope-product-portal.rb
@@ -0,0 +1,16 @@
+cask "izotope-product-portal" do
+  version "1.3.8"
+  sha256 "111db6bcef7702160449021c793959225a84c182fa9eaf069a0453c7d11f53a0"
+  url "https://s3.amazonaws.com/izotopedownloads/product_download/iZotope_Product_Portal_v#{version.major}_#{version.minor}_#{version.patch}.dmg"
+  name "iZotope Product Portal"
+  desc "Application that allows you to seamlessly manage your iZotope software in one central location"
+  homepage "https://support.izotope.com/hc/en-us/articles/360025586834-Welcome-to-iZotope-Product-Portal"
+
+  auto_updates true
+
+  installer script: {
+    executable: "Install Product Portal.app/Contents/MacOS/osx-x86_64",
+    args:       ["--mode", "unattended"],
+    sudo:       true,
+  }
+end
