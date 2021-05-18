970297208be631f762bd585bbbb7137179ab070a
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-18 08:45:01 +0300

    Bump izotope version

--- Casks/izotope-product-portal.rb
+++ Casks/izotope-product-portal.rb
@@ -1,6 +1,6 @@
 cask "izotope-product-portal" do
-  version "1.3.8"
-  sha256 "111db6bcef7702160449021c793959225a84c182fa9eaf069a0453c7d11f53a0"
+  version "1.4.1"
+  sha256 "d12c1630b5f9f73f4f32ce160e55d92d28daaf35051fc7a0549c2a2f9ebdf592"
 
   url "https://s3.amazonaws.com/izotopedownloads/product_download/iZotope_Product_Portal_v#{version.major}_#{version.minor}_#{version.patch}.dmg", verified: "amazonaws.com/izotopedownloads"
   name "iZotope Product Portal"
